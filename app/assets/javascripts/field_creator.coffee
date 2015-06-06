283448176e6174a9e068169815316cd82f820885
Kevin Hughes <kevin.hughes@shopify.com>
2015-06-06 16:14:28 -0400

    clean up fields controller

--- app/assets/javascripts/field_creator.coffee
+++ app/assets/javascripts/field_creator.coffee
@@ -33,19 +33,6 @@ class TournamentApp.FieldCreator
     @map.controls[google.maps.ControlPosition.TOP_RIGHT].push(input)
     google.maps.event.addDomListener input, 'click', @save
 
-  drawRectangles: =>
-    # rectangle = new google.maps.Rectangle({
-    #   strokeColor: '#FF0000',
-    #   strokeOpacity: 0.8,
-    #   strokeWeight: 2,
-    #   fillColor: '#FF0000',
-    #   fillOpacity: 0.35,
-    #   map: @map,
-    #   bounds: new google.maps.LatLngBounds(
-    #     new google.maps.LatLng(33.671068, -116.25128),
-    #     new google.maps.LatLng(33.685282, -116.233942))
-    # });
-
   save: =>
     data =
       tournament:
--- app/controllers/fields_controller.rb
+++ app/controllers/fields_controller.rb
@@ -1,63 +1,32 @@
 class FieldsController < ApplicationController
-  before_action :set_field, only: [:show, :edit, :update, :destroy]
+  before_action :load_tournament, only: [:index, :create]
 
-  # GET /fields
   def index
-    @fields = Field.all
   end
 
-  # GET /fields/1
-  def show
-  end
-
-  # GET /fields/new
-  def new
-    @tournament = Tournament.find(params[:tournament_id])
-  end
-
-  # GET /fields/1/edit
-  def edit
-  end
-
-  # POST /fields
   def create
-    @tournament = Tournament.find(params[:tournament_id])
-    @tournament.update_attributes(tournament_params)
-
-    redirect_to new_tournament_field_path(@tournament), notice: 'Fields updated'
-  end
-
-  # PATCH/PUT /fields/1
-  def update
-    if @field.update(field_params)
-      redirect_to @field, notice: 'Field was successfully updated.'
+    if @tournament.update_attributes(tournament_params)
+      head :ok
     else
-      render :edit
+      head :unprocessible_entity
     end
-  end
-
-  # DELETE /fields/1
-  def destroy
-    @field.destroy
-    redirect_to fields_url, notice: 'Field was successfully destroyed.'
   end
 
   private
-    def tournament_params
-      params.require(:tournament).permit(
-        :zoom,
-        :lat,
-        :long
-      )
-    end
 
-    # Use callbacks to share common setup or constraints between actions.
-    def set_field
-      @field = Field.find(params[:id])
-    end
+  def load_tournament
+    @tournament = Tournament.find(params[:tournament_id])
+  end
 
-    # Only allow a trusted parameter "white list" through.
-    def field_params
-      params.require(:field).permit(:name, :location)
-    end
+  def tournament_params
+    params.require(:tournament).permit(
+      :zoom,
+      :lat,
+      :long
+    )
+  end
+
+  def field_params
+    params.require(:field).permit(:name, :location)
+  end
 end
