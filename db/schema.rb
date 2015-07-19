14a422592cf6a1783afb5fcb4e3c916fbf83fe2c
Kevin Hughes <kevin.hughes@shopify.com>
2015-07-19 12:51:46 -0400

    push nice square coords to prod

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150718204032) do
+ActiveRecord::Schema.define(version: 20150719163116) do
 
   create_table "fields", force: :cascade do |t|
     t.string   "name"
