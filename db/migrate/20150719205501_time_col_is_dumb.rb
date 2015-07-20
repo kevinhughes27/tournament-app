519cff65f76a32c355c1d7fa5b95f49a55bd5e97
Kevin Hughes <kevin.hughes@shopify.com>
2015-07-19 23:50:35 -0400

    fix again

--- db/migrate/20150719205501_time_col_is_dumb.rb
+++ db/migrate/20150719205501_time_col_is_dumb.rb
@@ -1,6 +1,6 @@
 class TimeColIsDumb < ActiveRecord::Migration
   def change
     remove_column :tournaments, :time_cap
-    add_column :tournaments, :time_cap, :float
+    add_column :tournaments, :time_cap, :decimal, precision: 3
   end
 end
