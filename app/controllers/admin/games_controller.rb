30aa1cdbc93bfeb1b367530b3b047d93ccafb034
Kevin Hughes <kevin.hughes@shopify.com>
2015-07-25 19:13:32 -0400

    force int on score confirm

--- app/controllers/admin/games_controller.rb
+++ app/controllers/admin/games_controller.rb
@@ -6,13 +6,21 @@ class Admin::GamesController < AdminController
 
   def confirm_score
     game = Game.find(params[:id])
-    game.confirm_score(params[:home_score], params[:away_score])
+
+    home_score = params[:home_score].to_i
+    away_score = params[:away_score].to_i
+
+    game.confirm_score(home_score, away_score)
     redirect_to admin_tournament_score_reports_path(@tournament)
   end
 
   def update_score
     game = Game.find(params[:id])
-    game.update_score(params[:home_score], params[:away_score])
+
+    home_score = params[:home_score].to_i
+    away_score = params[:away_score].to_i
+
+    game.update_score(home_score,away_score)
     redirect_to admin_tournament_games_path(@tournament)
   end
 
