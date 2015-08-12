4a1eb9db4f33e643ca5fa9920310056f16da0190
Kevin Hughes <kevin.hughes@shopify.com>
2015-08-12 16:54:22 -0400

    add validations forcing game to have either both or none of field and start_time

--- app/models/game.rb
+++ app/models/game.rb
@@ -11,6 +11,9 @@ class Game < ActiveRecord::Base
   validates_presence_of :tournament
   validates_presence_of :bracket, :bracket_uid, :bracket_top, :bracket_bottom
 
+  validates_presence_of :field,      if: Proc.new{ |g| g.start_time.present? }
+  validates_presence_of :start_time, if: Proc.new{ |g| g.field.present? }
+
   after_save :update_bracket
 
   def winner
