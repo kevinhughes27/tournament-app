8a33bc476b44aadfec32494b2e9548220a982e1f
Kevin Hughes <kevin.hughes@shopify.com>
2015-08-15 15:47:48 -0400

    tournament validations and tests

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -9,8 +9,9 @@ class Tournament < ActiveRecord::Base
   has_many :games, dependent: :destroy
   has_many :score_reports, dependent: :destroy
 
-  validates_presence_of :handle
+  validates_presence_of :name, :handle, :time_cap
   validates_format_of   :handle, with: /\A[a-zA-Z0-9]+([\.\-\_]+[a-zA-Z0-9]+)*\Z/
+  validates_numericality_of :time_cap
 
   def app_link
     "/#{handle}"
--- test/models/tournament_test.rb
+++ test/models/tournament_test.rb
@@ -1,6 +1,24 @@
 require 'test_helper'
 
 class TournamentTest < ActiveSupport::TestCase
+  test "tournament requires a name" do
+    tournament = Tournament.new()
+    refute tournament.valid?
+    assert_equal ["can't be blank"], tournament.errors[:name]
+  end
+
+  test "tournament requires a handle" do
+    tournament = Tournament.new(name: 'No Borders')
+    refute tournament.valid?
+    assert_equal ["can't be blank", 'is invalid'], tournament.errors[:handle]
+  end
+
+  test "tournament requires a time cap" do
+    tournament = Tournament.new(name: 'No Borders', handle: 'no-borders')
+    refute tournament.valid?
+    assert_equal ["can't be blank", "is not a number"], tournament.errors[:time_cap]
+  end
+
   test "deleting a tournament deletes all its data" do
     tournament = tournaments(:noborders)
     tournament.destroy
