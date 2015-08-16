76091d19b9ad9edd848eb84836c2b4b60b9b47bd
Kevin Hughes <kevin.hughes@shopify.com>
2015-08-16 13:30:28 -0400

    score report server side turbolinks direct render (not working)

--- app/controllers/admin/score_reports_controller.rb
+++ app/controllers/admin/score_reports_controller.rb
@@ -1,19 +1,8 @@
 class Admin::ScoreReportsController < AdminController
 
   def index
-    scope = if params[:game_id]
-      @tournament.score_reports.where(game_id: params[:game_id])
-    else
-      @tournament.score_reports
-    end
-
-    @score_reports = scope.includes(game: [:home, :away]).order(:game_id)
-
-    if params[:change]
-      render :index, change: @score_reports.map{ |s| "score-report-#{s.id}"}
-    else
-      render :index
-    end
+    @score_reports = @tournament.score_reports.includes(game: [:home, :away]).order(:game_id)
+    render :index
   end
 
   def confirm
@@ -24,7 +13,9 @@ class Admin::ScoreReportsController < AdminController
     away_score = params[:away_score].to_i
 
     game.confirm_score(home_score, away_score)
-    redirect_to admin_tournament_score_reports_path(@tournament, game_id: report.game_id, change: true)
+
+    @score_reports = @tournament.score_reports.where(game_id: game.id)
+    render :index, change: @score_reports.map{ |s| "score-report-#{s.id}" }
   end
 
 end
