8649059cf29436e4ef3e3b7e7d72d2973edee9bf
Kevin Hughes <kevin.hughes@shopify.com>
2015-08-17 19:31:23 -0400

    add sanity validators for num teams and bracket progression

--- lib/bracket_template_validator.rb
+++ lib/bracket_template_validator.rb
@@ -30,8 +30,55 @@ class BracketTemplateValidator
     }
   }
 
-  def self.validate(template_json)
-    JSON::Validator.validate!(BRACKET_SCHEMA, template_json, strict: true)
-  end
+  class << self
+    def validate(template_json)
+      validate_schema(template_json) &&
+      validate_num_teams(template_json) &&
+      validate_first_round(template_json) &&
+      validate_progression(template_json)
+    end
 
+    def validate_schema(template_json)
+      JSON::Validator.validate!(BRACKET_SCHEMA, template_json, strict: true)
+    end
+
+    # validates that the number of teams is twice the
+    # number of games in round1
+    def validate_num_teams(template_json)
+      num_teams = template_json[:num_teams]
+      round1_games = template_json[:games].select{ |g| g[:round] == 1 }
+
+      num_teams / 2 == round1_games.size
+    end
+
+    # validates the seats in round1 are a set from 1 to N
+    def validate_first_round(template_json)
+      games = template_json[:games].select{ |g| g[:round] == 1 }
+
+      seats = games.inject([]) do |seats, game|
+        seats << game[:top].to_i
+        seats << game[:bottom].to_i
+        seats
+      end
+
+      seats.sort.each_with_index do |seat, idx|
+        return false unless seat == (idx+1)
+      end
+    end
+
+    # validates that the dependent games exist at least
+    # aka if I say a game has 'wq1' in the top then 'q1'
+    # needs to exist
+    def validate_progression(template_json)
+      uids = template_json[:games].map{ |g| g[:uid] }
+
+      non_seed_games = template_json[:games].select{ |g| g[:round] != 1 }
+      tops = non_seed_games.map{ |g| g[:top].gsub(/(w|l)/, '') }.uniq
+      bottoms = non_seed_games.map{ |g| g[:bottom].gsub(/(w|l)/, '') }.uniq
+
+      tops - uids == [] &&
+      bottoms - uids == []
+    end
+
+  end
 end
