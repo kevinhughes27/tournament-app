9b89b9dbbf478cd9f15bd62565a4e2d015475bec
Kevin Hughes <kevin.hughes@shopify.com>
2015-08-24 21:33:20 -0400

    remove old partials and rename admin lte ones

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,7 +2,7 @@ class AdminController < ApplicationController
   http_basic_authenticate_with name: "admin", password: "nobo"
 
   before_action :load_tournament
-  layout 'admin_lte_2'
+  layout 'admin'
 
   def load_tournament
     if params[:tournament_id]
