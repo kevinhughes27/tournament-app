f87d5b75de43070b3432bdf95a0ad4cf6426459c
Kevin Hughes <kevin.hughes@shopify.com>
2015-12-23 22:28:18 -0500

    re org code order

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,11 +6,6 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
 
-Rails.application.config.browserify_rails.commandline_options = [
-  "-t browserify-shim",
-  "-t babelify --extension='.jsx'",
-]
-
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += [
@@ -21,4 +16,9 @@ Rails.application.config.assets.precompile += [
   'app.css',
   'app.js',
   'fingerblast.js'
+]
+
+Rails.application.config.browserify_rails.commandline_options = [
+  "-t browserify-shim",
+  "-t babelify --extension='.jsx'",
 ]
