e13ca12f5cc3594bab3eba2f0126104e91690618
Kevin Hughes <kevin.hughes@shopify.com>
2015-12-23 22:21:50 -0500

    move routes patch to intializers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
-class ActionDispatch::Routing::Mapper
-  def draw(routes_name)
-    instance_eval(File.read(Rails.root.join("config/routes/#{routes_name}.rb")))
-  end
-end
-
 Rails.application.routes.draw do
   root 'landing#index'
 
