a01d9ced36f7437593fa751d4dc1b4b215aff498
Kevin Hughes <kevin.hughes@shopify.com>
2015-12-26 18:20:49 -0500

    bring root/vendor into assets folder and manage locally with bower (files not ignored)

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -3,18 +3,16 @@
 //= require twine
 //= require turbolinks
 
-//= require moment
-//= require underscore
-
 //= require jquery.cookie
-//= require datetimepicker
 //= require jstz
 //= require browser_timezone_rails/set_time_zone
-//= require sticky-tabs
+
 //= require interact
 //= require bootstrap-sprockets
 
-//= require ./admin/admin
-//= require ./admin/components
+//= require admin/vendor
+
+//= require admin/admin
+//= require admin/components
 //= require_tree ./admin/modules
 //= require_tree ./admin/initializers
--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,18 +1,15 @@
 //= require jquery
+//= require jquery_ujs
 //= require twine
 //= require turbolinks
 
-//= require moment
-//= require underscore
-
-//= require jquery.scrollTo
 //= require jquery.cookie
 //= require jstz
 //= require browser_timezone_rails/set_time_zone
-//= require awesomplete
 
-//= require fingerprintjs2
+//= require app/vendor
 
-//= require_self
-//= require_tree ./app
-window.App = window.App || {};
+//= require app/app
+//= require_tree ./app/lib
+//= require_tree ./app/initializers
+//= require_tree ./app/modules
