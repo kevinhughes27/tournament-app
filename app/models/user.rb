873446ad0d49477f7f9eb9bc68de13ada748e23f
Kevin Hughes <kevin.hughes@shopify.com>
2016-01-01 15:59:08 -0500

    style the user panel in the sidebar

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,18 @@ class User < ActiveRecord::Base
          :rememberable,
          :trackable,
          :validatable
+
+  def name
+    email.split('@').first
+  end
+
+  def gravatar_url
+    "http://www.gravatar.com/avatar/#{gravatar_hash}?s=200&d=mm"
+  end
+
+  private
+
+  def gravatar_hash
+    email.present? ? Digest::MD5.hexdigest(email) : ''
+  end
 end
