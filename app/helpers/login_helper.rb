93f4ee5a57ed9ca1fa82391985f1a06525ec6314
Kevin Hughes <kevin.hughes@shopify.com>
2016-01-10 15:33:31 -0500

    remove unauth flash and update login animation helper

--- app/helpers/login_helper.rb
+++ app/helpers/login_helper.rb
@@ -1,6 +1,6 @@
 module LoginHelper
   def login_animation
-    if flash[:alert] && flash[:alert] != 'You need to sign in or sign up before continuing.'
+    if flash[:alert].present?
       'pulse'
     else
       'fadeInDown'
