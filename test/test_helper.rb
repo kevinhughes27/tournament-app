98301c47405ed19c4c81b4cb3b0712d2054ecfc4
Kevin Hughes <kevin.hughes@shopify.com>
2016-01-12 17:54:07 -0500

    change local output of test coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,9 @@ require 'simplecov'
 if ENV['CIRCLE_ARTIFACTS']
   dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
   SimpleCov.coverage_dir(dir)
+else
+  dir = File.join(File.dirname(__FILE__), "coverage")
+  SimpleCov.coverage_dir(dir)
 end
 
 SimpleCov.start 'rails'
