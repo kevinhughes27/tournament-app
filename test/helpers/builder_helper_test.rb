80e9777704a0e3062441aa0565da18fee1b3ffd6
Kevin Hughes <kevin.hughes@shopify.com>
2016-01-11 21:04:15 -0500

    add builder helper tests

---
+++ test/helpers/builder_helper_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class BuilderHelperTest < ActionView::TestCase
+
+  test "builder card default animation is bounceInRight" do
+    card = builder_card{ "test" }
+    assert_match /bounceInRight/, card
+  end
+
+  test "builder card animation is pulse if there is an error" do
+    card = builder_card(error: true){ "test" }
+    assert_match /pulse/, card
+  end
+
+  test "builder card animation is bounceInLeft if navigation is back" do
+    card = builder_card(error: false, back: true){ "test" }
+    assert_match /bounceInLeft/, card
+  end
+
+end
