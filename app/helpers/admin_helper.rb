23c2866e946633b92e34ea29c7dad42834250663
Kevin Hughes <kevin.hughes@shopify.com>
2016-01-20 22:15:19 -0500

    renamne admin content helper

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,6 +1,6 @@
 module AdminHelper
 
-  def admin_content_box(&block)
+  def admin_content(&block)
     content_tag(:section, class: "content") do
       content_tag(:div, class: "box") do
         content_tag(:div, class: "box-body") { yield }
