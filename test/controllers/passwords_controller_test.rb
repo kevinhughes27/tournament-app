f43302f776e0a87d1fe6ab5bc92efac9df5f2421
Kevin Hughes <kevin.hughes@shopify.com>
2016-01-24 13:09:53 -0500

    add tests (mainly for redirects after devise does the work)

---
+++ test/controllers/passwords_controller_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class PasswordsControllerTest < ActionController::TestCase
+
+  setup do
+    @user = users(:kevin)
+    @tournament = tournaments(:noborders)
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+  end
+
+  test "get new (aka password reset)" do
+    get :new
+    assert_response :ok
+  end
+
+  test "post create (aka password reset)" do
+    post :create, user: {email: @user.email}
+    assert_redirected_to new_user_session_path
+  end
+
+  test "get edit (aka change password)" do
+    get :edit, reset_password_token: 'token'
+    assert_response :ok
+  end
+
+  test "put update (aka change password)" do
+    token = @user.send_reset_password_instructions
+    put :update, user: {password: 'password', password_confirmation:'password', reset_password_token: token}
+    assert_redirected_to new_user_session_path
+  end
+
+end
