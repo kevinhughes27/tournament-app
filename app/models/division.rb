85ebd951707eb80a0161ab7c96f71b29a6aff69d
Kevin Hughes <kevin.hughes@shopify.com>
2016-02-26 01:40:38 -0500

    create a job for the dirty seed check operation

--- app/models/division.rb
+++ app/models/division.rb
@@ -17,41 +17,8 @@ class Division < ActiveRecord::Base
     @bracket ||= Bracket.find_by(name: self.bracket_type)
   end
 
-  # returns true if seeding would result in changes
-  # only for initial seed
   def dirty_seed?
-    return true if self.teams.blank?
-    return true unless seeded?
-
-    teams = self.teams.order(:seed)
-    seeds = self.teams.pluck(:seed).sort
-
-    seeds.each_with_index do |seed, idx|
-      return true unless seed == (idx+1)
-    end
-
-    game_uids = bracket.game_uids_for_round(1)
-    games = Game.where(division_id: id, bracket_uid: game_uids)
-
-    return true unless games.all?{ |g| g.valid_for_seed_round? }
-
-    seats = games.pluck(:home_prereq_uid, :away_prereq_uid).flatten.uniq
-    seats.reject!{ |s| !s.to_s.is_i? }
-    num_seats = seats.size
-
-    return true unless num_seats == teams.size
-
-    games.each do |game|
-      if game.home_prereq_uid.is_i?
-        return true if game.home != teams[game.home_prereq_uid.to_i - 1]
-      end
-
-      if game.away_prereq_uid.is_i?
-        return true if game.away != teams[game.away_prereq_uid.to_i - 1]
-      end
-    end
-
-    return false
+    DirtySeedJob.perform_now(division: self)
   end
 
   def seeded?
