0f1655579909fe3c52256c0bdf0c35fd767eb86a
Kevin Hughes <kevin.hughes@shopify.com>
2016-03-04 22:58:07 -0500

    basic visualtion working

--- Gemfile
+++ Gemfile
@@ -33,7 +33,6 @@ gem 'uglifier', '>= 1.3.0'
 gem 'sass-rails', '~> 5.0'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
-
 gem 'interact-rails'
 
 # frontend frameworks
--- app/assets/javascripts/admin/vendor.js
+++ app/assets/javascripts/admin/vendor.js
@@ -4,3 +4,4 @@
 //= require vendor/leaflet/dist/leaflet
 //= require vendor/leaflet.editable/src/Leaflet.Editable
 //= require vendor/leaflet-search/src/leaflet-search
+//= require vendor/vis/dist/vis
