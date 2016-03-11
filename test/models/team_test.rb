7d55cf404e404f47ace0179a37d9e147b14861be
Kevin Hughes <kevin.hughes@shopify.com>
2016-03-10 23:56:06 -0500

    add tests for team unassign games

--- test/models/team_test.rb
+++ test/models/team_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class TeamTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @team = teams(:swift)
+    @game = games(:swift_goose)
+  end
+
+  test "deleting a team unassigns if from all games (home)" do
+    assert_equal @team, @game.home
+    @team.destroy
+    assert_nil @game.reload.home
+  end
+
+  test "deleting a team unassigns if from all games (away)" do
+    team = teams(:goose)
+    assert_equal team, @game.away
+    team.destroy
+    assert_nil @game.reload.away
+  end
 end
