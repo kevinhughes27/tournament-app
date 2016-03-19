dfd65640cd93242e2ba4029a69edd71ef24d0b69
Kevin Hughes <kevin.hughes@shopify.com>
2016-03-19 14:28:19 -0400

    fix racey browser test (would fill in signup fields not login fields since it didn't wait)

--- test/browser/login_browser_test.rb
+++ test/browser/login_browser_test.rb
@@ -15,6 +15,7 @@ class LoginBrowserTest < BrowserTest
     visit("http://#{@domain}/")
     click_on('Log in')
 
+    assert_text 'Log in to manage your tournament'
     fill_in('user_email', with: @user.email)
     fill_in('user_password', with: 'password')
     click_on('Log in')
