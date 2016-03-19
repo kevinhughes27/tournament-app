6957edf1e5188349fa007bb4539e98bbe3db368b
Kevin Hughes <kevin.hughes@shopify.com>
2016-03-19 14:58:01 -0400

    add limit to tournament users

--- test/models/tournament_user_test.rb
+++ test/models/tournament_user_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class TournamentUserTest < ActiveSupport::TestCase
+  setup do
+    @tournament = tournaments(:noborders)
+  end
 
   test "tournament_users are unique" do
     user = users(:kevin)
@@ -13,4 +16,15 @@ class TournamentUserTest < ActiveSupport::TestCase
     assert_equal ['has already been taken'], tournament_user.errors[:user]
   end
 
+  test "limited number of users per tournament" do
+    stub_constant(TournamentUser, :LIMIT, 1) do
+      user = @tournament.tournament_users.build(user: users(:sam))
+      refute user.valid?
+      assert_equal ['Maximum of 1 users exceeded'], user.errors[:base]
+    end
+  end
+
+  test "limit is define" do
+    assert_equal 32, TournamentUser::LIMIT
+  end
 end
