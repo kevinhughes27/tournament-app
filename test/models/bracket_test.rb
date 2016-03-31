e0cf2bc479b0fe15ddae19534d650fce37305348
Kevin Hughes <kevin.hughes@shopify.com>
2016-03-30 23:02:45 -0400

    update tests for bracket handle update

--- test/models/bracket_test.rb
+++ test/models/bracket_test.rb
@@ -2,14 +2,14 @@ require 'test_helper'
 
 class BracketTest < ActiveSupport::TestCase
   test "game_uids_for_round returns the uids for the given round" do
-    bracket = Bracket.find_by(name: 'single_elimination_8')
+    bracket = Bracket.find_by(handle: 'single_elimination_8')
     assert_equal ['q1', 'q2', 'q3', 'q4'], bracket.game_uids_for_round(1)
   end
 
   BracketTemplateValidator.methods(false).each do |validation|
     next unless validation.to_s.include? 'validate_'
     Bracket.all.each do |bracket|
-      test "bracket template #{validation} #{bracket.name}" do
+      test "bracket template #{validation} #{bracket.handle}" do
         assert BracketTemplateValidator.send(validation, bracket.template)
       end
     end
