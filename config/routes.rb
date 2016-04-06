c588c3e996c25e66d1e5a4bc7d2b00690932b364
Kevin Hughes <kevin.hughes@shopify.com>
2016-04-05 23:25:20 -0400

    update login url

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users,
     path: '',
+    path_names: { sign_in: 'log_in' },
     controllers: { sessions: 'login', omniauth_callbacks: 'omniauth_callbacks', passwords: 'passwords' },
     skip: [:registration]
 
