ebf2aa3be291fa9a8bfee7a8619d78dfc694539c
Kevin Hughes <kevin.hughes@shopify.com>
2016-04-06 21:56:22 -0400

    burn smooth scroll

--- app/assets/javascripts/brochure/vendor.js
+++ app/assets/javascripts/brochure/vendor.js
@@ -1,5 +1,4 @@
 //= require vendor/jquery.scrollTo/jquery.scrollTo
 //= require vendor/animatedmodal/animatedModal
-//= require vendor/SmoothScroll
 //= require vendor/underscore/underscore
 //= require vendor/leaflet/dist/leaflet
