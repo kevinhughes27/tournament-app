e0ec7e0aaf485862a0c962e36802e7b0d5b4e4ef
Kevin Hughes <kevin.hughes@shopify.com>
2016-05-01 16:22:05 -0400

    fix bracket selection

--- app/helpers/divisions_helper.rb
+++ app/helpers/divisions_helper.rb
@@ -2,7 +2,7 @@ module DivisionsHelper
   def bracket_options(division)
     options_for_select(
       Bracket.where(num_teams: division.num_teams, days: division.num_days).pluck(:name, :handle),
-      division.bracket.try(:name)
+      division.bracket.try(:handle)
     )
   end
 end
