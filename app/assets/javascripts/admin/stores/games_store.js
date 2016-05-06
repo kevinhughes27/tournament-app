145203b8fb3dab2ca286ee12aba30934f1a2960b
Kevin Hughes <kevin.hughes@shopify.com>
2016-05-05 22:38:22 -0400

    make some methods private on GamesStore

--- app/assets/javascripts/admin/stores/games_store.js
+++ app/assets/javascripts/admin/stores/games_store.js
@@ -22,40 +22,40 @@ var GamesStore = _.extend({}, Store, {
   },
 
   saveReportsState(game, state) {
-    var idx = this._findGameIdx(game);
+    var idx = findGameIdx(game);
     _games[idx].reportsOpen = state;
   },
 
   updateGame(game) {
-    var idx = this._findGameIdx(game);
+    var idx = findGameIdx(game);
     if(idx == -1) { return; }
 
     if(!_games[idx].confirmed && game.confirmed) {
-      this._updateSidebarBadge();
+      updateSidebarBadge();
     }
 
     _.extend(_games[idx], game);
     this.emitChange();
-  },
-
-  _findGameIdx(game) {
-    var idx = _.findIndex(_games, function(g) {
-      return g.id == game.id;
-    });
-
-    return idx;
-  },
-
-  _updateSidebarBadge() {
-    var $node = $('#games-badge');
-    var count = parseInt($node.text());
-    count = count - 1;
-    if(count == 0) {
-      $node.hide();
-    } else {
-      $node.text(count);
-    }
   }
 });
+
+var findGameIdx = function(game) {
+  var idx = _.findIndex(_games, function(g) {
+    return g.id == game.id;
+  });
+
+  return idx;
+};
+
+var updateSidebarBadge = function() {
+  var $node = $('#games-badge');
+  var count = parseInt($node.text());
+  count = count - 1;
+  if(count == 0) {
+    $node.hide();
+  } else {
+    $node.text(count);
+  }
+};
 
 module.exports = GamesStore;
