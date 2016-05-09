1a94848d9554b8ff9abe3a041110c0365500b102
Kevin Hughes <kevin.hughes@shopify.com>
2016-05-08 20:39:47 -0400

    add kaminari pagination to tournaments internal

--- app/controllers/internal/tournaments_controller.rb
+++ app/controllers/internal/tournaments_controller.rb
@@ -1,5 +1,11 @@
 class Internal::TournamentsController < InternalController
+  PER_PAGE = 25
+
   def index
-    @tournaments = Tournament.all.includes(:tournament_users, :users).order(created_at: :desc)
+    @tournaments = Tournament.all
+                    .includes(:tournament_users, :users)
+                    .page(params[:page])
+                    .per(PER_PAGE)
+                    .order(created_at: :desc)
   end
 end
