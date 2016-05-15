12730d48e9c6c85dff050e17ddb7482079397bd4
Kevin Hughes <kevin.hughes@shopify.com>
2016-05-15 11:15:09 -0600

    add games controller update safety tests

--- test/controllers/admin/games_controller_test.rb
+++ test/controllers/admin/games_controller_test.rb
@@ -34,6 +34,40 @@ class Admin::GamesControllerTest < ActionController::TestCase
     assert_equal 13, @game.away_score
   end
 
+  test "update updates the games score (unsafe)" do
+    games(:semi_final).update_attributes!(
+      division: @game.division,
+      score_confirmed: true
+    )
+
+    put :update, params: {
+      id: @game.id,
+      home_score: @game.away_score,
+      away_score: @game.home_score
+    }, format: :json
+
+    assert_response :unprocessable_entity
+  end
+
+  test "update updates the games score (unsafe) + force" do
+    games(:semi_final).update_attributes!(
+      division: @game.division,
+      score_confirmed: true
+    )
+
+    put :update, params: {
+      id: @game.id,
+      home_score: @game.away_score,
+      away_score: @game.home_score,
+      force: 'true'
+    }, format: :json
+
+    assert_response :ok
+
+    assert_equal 1, @game.reload.home_score
+    assert_equal 2, @game.away_score
+  end
+
   # TODO make this test work
   # test "updating scores broadcasts changes" do
   #   put :update, params: {
