4186a1fae9c94b24dd40b78987e6f9ee9fb49eba
Kevin Hughes <kevin.hughes@shopify.com>
2016-05-31 23:32:17 -0400

    tone down concurrency

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,8 @@
 if Rails.env.production?
+  Sidekiq.configure_client do |config|
+    config.redis = { url: ENV["REDIS_URL"], size: 3 }
+  end
+
   Sidekiq.configure_server do |config|
     config.redis = { url: ENV['REDIS_URL'] }
   end
