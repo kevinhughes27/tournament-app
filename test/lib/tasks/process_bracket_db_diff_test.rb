14059bbeeb3946252aa3e0de06e1f041fda27991
Kevin Hughes <kevin.hughes@shopify.com>
2016-06-14 22:51:02 -0400

    comment out bracket diff task test

--- test/lib/tasks/process_bracket_db_diff_test.rb
+++ test/lib/tasks/process_bracket_db_diff_test.rb
@@ -1,24 +1,25 @@
 require 'test_helper'
 
 class ProcessBracketDbDiffTaskTest < MaintenanceTaskTextCase
-  test "running task updates games" do
-    tournament = tournaments(:noborders)
-    division = divisions(:open)
-
-    # representative old data
-    game = Game.create!(
-      tournament: tournament,
-      division: division,
-      round: 3,
-      bracket_uid: 'f1',
-      home_prereq_uid: 'Ws1',
-      away_prereq_uid: 'Ws2'
-    )
-
-    subject.invoke
-
-    game.reload
-    assert_equal '1', game.bracket_uid
-    assert_equal 3, game.round
-  end
+  # diff to update uids for finals to numbers to save letters
+  # test "running task updates games" do
+  #   tournament = tournaments(:noborders)
+  #   division = divisions(:open)
+  #
+  #   # representative old data
+  #   game = Game.create!(
+  #     tournament: tournament,
+  #     division: division,
+  #     round: 3,
+  #     bracket_uid: 'f1',
+  #     home_prereq_uid: 'Ws1',
+  #     away_prereq_uid: 'Ws2'
+  #   )
+  #
+  #   subject.invoke
+  #
+  #   game.reload
+  #   assert_equal '1', game.bracket_uid
+  #   assert_equal 3, game.round
+  # end
 end
