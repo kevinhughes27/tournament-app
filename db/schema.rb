d6bb2fdb1c4c423dfaa6baebb4b230acc231d1f7
Kevin Hughes <kevin.hughes@shopify.com>
2016-06-19 21:31:49 -0400

    upgrade all brackets to the full version

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160616233831) do
+ActiveRecord::Schema.define(version: 20160620012517) do
 
   create_table "divisions", force: :cascade do |t|
     t.integer  "tournament_id"
