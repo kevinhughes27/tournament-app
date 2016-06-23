0e35e91a7ae5faea90553a84bacc349bd281db63
Kevin Hughes <kevin.hughes@shopify.com>
2016-06-23 13:32:11 -0400

    refactor some smaller methods into my auth controllers

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,15 +1,15 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def callback
-    user = User.from_omniauth(request.env['omniauth.auth'])
+    user = User.from_omniauth(auth_hash)
     sign_in(:user, user)
 
-    if omniauth_origin.include?(new_internal_user_session_path) && current_user.staff?
+    if for_internal_area?
       sign_in(:internal_user, user)
       return redirect_to internal_url
     end
 
-    if user.sign_in_count == 1 || !user.tournaments.exists?
+    if user_needs_setup?(user)
       redirect_to setup_path
     else
       flash[:animate] = "fadeIn"
@@ -17,27 +17,38 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
   end
 
-  ['google_oauth2', 'facebook'].each do |k|
-    alias_method k, :callback
-  end
+  alias_method :google_oauth2, :callback
+  alias_method :facebook, :callback
 
   private
 
   def after_sign_in_path(user)
-    if tournament_subdomain
-      return admin_url(subdomain: tournament_subdomain) + stored_path
+    if for_tournament_subdomain?
+      return admin_url(subdomain: subdomain) + stored_path
     end
 
-    if user.tournaments.count == 1
+    if user_has_multiple_tournaments?(user)
+      choose_tournament_path
+    else
       tournament = user.tournaments.first
       admin_url(subdomain: tournament.handle)
-    else
-      choose_tournament_path
     end
   end
 
-  def tournament_subdomain
-    subdomain unless subdomain == 'www'
+  def user_needs_setup?(user)
+    user.sign_in_count == 1 || !user.tournaments.exists?
+  end
+
+  def user_has_multiple_tournaments?(user)
+    user.tournaments.count > 1
+  end
+
+  def for_internal_area?
+    omniauth_origin.include?(new_internal_user_session_path) && current_user.staff?
+  end
+
+  def for_tournament_subdomain?
+    subdomain && subdomain != 'www'
   end
 
   def stored_path
@@ -47,6 +58,10 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def subdomain
     parts = URI.parse(omniauth_origin).host.split('.')
     parts.size == 3 ? parts.first : nil
+  end
+
+  def auth_hash
+    request.env['omniauth.auth']
   end
 
   def omniauth_origin
