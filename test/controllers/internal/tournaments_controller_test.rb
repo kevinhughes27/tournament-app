ca2ee09ad4a4a5bccf3ec26404cb6d772f4b97e9
Kevin Hughes <kevin.hughes@shopify.com>
2016-06-28 22:39:03 -0400

    fix devise dep notices

--- test/controllers/internal/tournaments_controller_test.rb
+++ test/controllers/internal/tournaments_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class Internal::TournamentsControllerTest < ActionController::TestCase
   setup do
-    sign_in :internal_user, users(:kevin)
+    sign_in users(:kevin), scope: :internal_user
   end
 
   test "get index" do
