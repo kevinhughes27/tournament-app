1e866eda5fd5c119290d397e2a8f2bfe2c56ff32
Kevin Hughes <kevin.hughes@shopify.com>
2016-07-04 21:54:38 -0400

    refactor BracketDb namespace and function names

--- lib/bracket_db/utils.rb
+++ lib/bracket_db/utils.rb
@@ -1,7 +1,12 @@
-def load_bracket(name)
-  Tilt::ERBTemplate.new("db/brackets/#{name}.json").render
-end
+class BracketDb
+  class << self
+    def load_bracket(name)
+      Tilt::ERBTemplate.new("db/brackets/#{name}.json").render
+    end
 
-def bracket_partial(name, args = nil)
-  Tilt::ERBTemplate.new("db/brackets/partials/#{name}.json").render(Object.new, args)
+    def partial(name, args = nil)
+      Tilt::ERBTemplate.new("db/brackets/partials/#{name}.json").render(Object.new, args)
+    end
+    alias_method :table_partial, :partial
+  end
 end
