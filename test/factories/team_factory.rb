c3a201fa03b6f5881a4846bae552df4df3fb3b51
Kevin Hughes <kevinhughes27@gmail.com>
2017-07-07 16:21:06 -0400

    update mailer tests

--- test/factories/team_factory.rb
+++ test/factories/team_factory.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :team do
     tournament { Tournament.first || FactoryGirl.build(:tournament) }
     name { Faker::Team.name }
+    email { Faker::Internet.email }
     sequence(:seed)
   end
 end
