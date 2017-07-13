02b707a456286676e6965012ba9b778c798d4eea
Kevin Hughes <kevinhughes27@gmail.com>
2017-07-12 23:02:15 -0400

    update schedule controller tests and add blank slates back to the rails index template

--- app/controllers/admin/schedule_controller.rb
+++ app/controllers/admin/schedule_controller.rb
@@ -41,28 +41,28 @@ class Admin::ScheduleController < AdminController
     head :ok
   end
 
-  def bulk_update
-    ActiveRecord::Base.transaction do
-      # reset field and start times first so swapping can work.
-      # Otherwise it might trigger conflict validations.
-      game_ids = games_params.map{ |p| p[:id].to_i }
-      games = Game.where(tournament_id: @tournament.id, id: game_ids)
-      games.update_all(field_id: nil, start_time: nil)
-
-      games_params.each do |p|
-        @game = Game.find_by(tournament_id: @tournament.id, id: p[:id])
-        if p[:field_id].present? && p[:start_time].present?
-          ScheduleGame.perform(@game, p[:field_id], p[:start_time])
-        end
-      end
-    end
-
-    load_index_data
-    render :index
-
-  rescue => e
-    render json: {game_id: @game.id, error: e.message}, status: :unprocessable_entity
-  end
+  # def bulk_update
+  #   ActiveRecord::Base.transaction do
+  #     # reset field and start times first so swapping can work.
+  #     # Otherwise it might trigger conflict validations.
+  #     game_ids = games_params.map{ |p| p[:id].to_i }
+  #     games = Game.where(tournament_id: @tournament.id, id: game_ids)
+  #     games.update_all(field_id: nil, start_time: nil)
+  #
+  #     games_params.each do |p|
+  #       @game = Game.find_by(tournament_id: @tournament.id, id: p[:id])
+  #       if p[:field_id].present? && p[:start_time].present?
+  #         ScheduleGame.perform(@game, p[:field_id], p[:start_time])
+  #       end
+  #     end
+  #   end
+  #
+  #   load_index_data
+  #   render :index
+  #
+  # rescue => e
+  #   render json: {game_id: @game.id, error: e.message}, status: :unprocessable_entity
+  # end
 
   private
 
