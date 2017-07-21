c774d3869a8004371432803e42cf0de9ba483a18
Kevin Hughes <kevinhughes27@gmail.com>
2017-07-20 23:05:08 -0400

    fix bulk action test for default division issue

--- test/operations/bulk_actions/set_teams_division_test.rb
+++ test/operations/bulk_actions/set_teams_division_test.rb
@@ -9,35 +9,37 @@ module BulkActions
 
     test "assigns teams to divisions" do
       team = FactoryGirl.create(:team)
+      new_division = FactoryGirl.create(:division)
 
       action = SetTeamsDivision.new(
         tournament_id: @tournament.id,
         ids: [team.id],
-        arg: @division.name
+        arg: new_division.name
       )
       action.perform
 
       assert_equal 200, action.status
       json = JSON.parse(action.response)
       assert_equal team.name, json.first["name"]
-      assert_equal @division.name, json.first["division"]
-      assert_equal @division, team.reload.division
+      assert_equal new_division.name, json.first["division"]
+      assert_equal new_division, team.reload.division
     end
 
     test "fails if it is not safe to change divisions" do
       team = FactoryGirl.create(:team)
+      new_division = FactoryGirl.create(:division)
       FactoryGirl.create(:game, :finished, home: team)
 
       action = SetTeamsDivision.new(
         tournament_id: @tournament.id,
         ids: [team.id],
-        arg: @division.name
+        arg: new_division.name
       )
       action.perform
 
       assert_equal 422, action.status
       assert_equal 'Cancelled: not all teams could be updated safely', action.response[:message]
-      refute_equal @division, team.reload.division
+      refute_equal new_division, team.reload.division
     end
   end
 end
