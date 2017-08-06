32cf42b37c54dff8327620f88a581bfe0677161f
Kevin Hughes <kevinhughes27@gmail.com>
2017-08-06 16:52:17 -0400

    wip bottom navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
-import AppBar from 'material-ui/AppBar';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import CircularProgress from 'material-ui/CircularProgress';
 import Center from 'react-center';
 import { connect } from 'react-redux';
 import ScheduleList from './ScheduleList';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import {
+  BottomNavigation,
+  BottomNavigationItem
+} from 'material-ui/BottomNavigation';
+import CircularProgress from 'material-ui/CircularProgress';
+import FontIcon from 'material-ui/FontIcon';
+import IconLocationOn from 'material-ui/svg-icons/communication/location-on';
+
+const recentsIcon = <FontIcon className="material-icons">restore</FontIcon>;
+const favoritesIcon = <FontIcon className="material-icons">favorite</FontIcon>;
+const nearbyIcon = <IconLocationOn />;
+
 class App extends Component {
+  state = {
+    selectedIndex: 0
+  };
+
+  select = index => this.setState({ selectedIndex: index });
+
   render() {
     return (
       <MuiThemeProvider style={{ height: '100%' }}>
         <div>
           <AppBar title="Ultimate Tournament" />
           {renderContent(this.props)}
+          <BottomNavigation selectedIndex={this.state.selectedIndex}>
+            <BottomNavigationItem
+              label="Recents"
+              icon={recentsIcon}
+              onTouchTap={() => this.select(0)}
+            />
+            <BottomNavigationItem
+              label="Favorites"
+              icon={favoritesIcon}
+              onTouchTap={() => this.select(1)}
+            />
+            <BottomNavigationItem
+              label="Nearby"
+              icon={nearbyIcon}
+              onTouchTap={() => this.select(2)}
+            />
+          </BottomNavigation>
         </div>
       </MuiThemeProvider>
     );
