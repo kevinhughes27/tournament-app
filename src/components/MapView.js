731b5325fade9094aa81c759aa110233986a1803
Kevin Hughes <kevinhughes27@gmail.com>
2017-08-15 19:22:37 -0400

    add the fields to the MapView using GeoJson layers

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, GeoJSON } from 'react-leaflet';
 
 class MapView extends Component {
   render() {
     const { lat, long, zoom } = window.tournament.map;
+    const fields = this.props.fields;
 
     return (
       <Map center={[lat, long]} zoom={zoom} zoomControl={false}>
@@ -13,12 +14,19 @@ class MapView extends Component {
           url="https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
           subdomains={['mt0', 'mt1', 'mt2', 'mt3']}
         />
+        {renderFields(fields)}
       </Map>
     );
   }
 }
 
+function renderFields(fields) {
+  return fields.map(field => {
+    return <GeoJSON key={field.id} data={JSON.parse(field.geo_json)} />;
+  });
+}
+
 export default connect(state => ({
-  games: state.app.games,
+  fields: state.app.fields,
   search: state.app.search
 }))(MapView);
