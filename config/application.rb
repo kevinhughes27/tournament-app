7b6aca1a235d2b0b0471d71a58a11dba8a881e1a
Kevin Hughes <kevinhughes27@gmail.com>
2017-09-01 19:48:24 -0400

    remove rack-cors now that webpack dev proxy is configured correctly

--- config/application.rb
+++ config/application.rb
@@ -8,13 +8,6 @@ require_relative '../lib/subdomain'
 
 module UltimateTournament
   class Application < Rails::Application
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
-      end
-    end
-
     config.active_record.default_timezone = :local
 
     config.generators do |generate|
