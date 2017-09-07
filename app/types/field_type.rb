1bae5b26e2a4edf2e669bef507cf1fa17cee638c
Kevin Hughes <kevinhughes27@gmail.com>
2017-09-06 23:17:50 -0400

    add lat and long to field type

--- app/types/field_type.rb
+++ app/types/field_type.rb
@@ -3,5 +3,7 @@ FieldType = GraphQL::ObjectType.define do
   description "A Field"
   field :id, types.ID
   field :name, types.String
+  field :lat, types.Float
+  field :long, types.Float
   field :geo_json, types.String
 end
