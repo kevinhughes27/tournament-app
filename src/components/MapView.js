aa2f13fe05993042cec9c8cbde715ef0c0740f3f
Kevin Hughes <kevinhughes27@gmail.com>
2017-09-06 22:29:17 -0400

    find currentGame implementation

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Map, TileLayer, GeoJSON } from 'react-leaflet';
 import gamesSearch from '../helpers/gamesSearch';
+import moment from 'moment';
+import _filter from 'lodash/filter';
 
 class MapView extends Component {
   render() {
     const { map: { lat, long, zoom }, fields } = this.props;
+
     const { search, games } = this.props;
     const filteredGames = gamesSearch(search, games);
+
+    const currentTime = moment();
+    const currentGame = _filter(filteredGames, game => {
+      const startTime = moment(game.start_time);
+      const endTime = moment(game.end_time);
+      return currentTime.isBetween(startTime && endTime);
+    });
 
     return (
       <Map center={[lat, long]} zoom={zoom} zoomControl={false}>
