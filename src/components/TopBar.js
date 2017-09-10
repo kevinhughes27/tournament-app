d5e592451ad54c4b6031f0e393eb4c7435c76787
Kevin Hughes <kevinhughes27@gmail.com>
2017-09-10 00:24:54 -0400

    remove useless topbar styles and work on top bar a bit

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -2,57 +2,39 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
-import { withStyles } from 'material-ui/styles';
+import IconButton from 'material-ui/IconButton';
 import SearchIcon from 'material-ui-icons/Search';
 import AutoComplete from './AutoComplete';
 import Typography from 'material-ui/Typography';
-
-const styles = {
-  root: {
-    width: '100%'
-  }
-};
 
 class TopBar extends Component {
   render() {
     const { teams = [], search, dispatch } = this.props;
     const teamNames = teams.map(t => t.name);
 
-    const classes = this.props.classes;
-
     return (
-      <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar>
+      <AppBar>
+        <Toolbar>
+          <IconButton color="contrast">
             <SearchIcon />
-            <Typography type="title" color="inherit">
-              <AutoComplete
-                value={search}
-                placeholder="Search Teams"
-                suggestions={teamNames}
-                onChange={search => {
-                  dispatch({ type: 'SET_SEARCH', value: search });
-                }}
-              />
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </div>
+          </IconButton>
+          <Typography type="title" color="inherit">
+            <AutoComplete
+              value={search}
+              placeholder="Search Teams"
+              suggestions={teamNames}
+              onChange={search => {
+                dispatch({ type: 'SET_SEARCH', value: search });
+              }}
+            />
+          </Typography>
+        </Toolbar>
+      </AppBar>
     );
-    // return (
-    //   <AppBar position="static">
-    //     <Toolbar disableGutters>
-    //       <SearchIcon />
-    //
-    //     </Toolbar>
-    //   </AppBar>
-    // );
   }
 }
 
-export default withStyles(styles)(
-  connect(state => ({
-    teams: state.app.teams,
-    search: state.app.search
-  }))(TopBar)
-);
+export default connect(state => ({
+  teams: state.app.teams,
+  search: state.app.search
+}))(TopBar);
