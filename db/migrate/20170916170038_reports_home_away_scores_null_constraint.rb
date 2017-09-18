a91affe38dfb02e58f5aa0055684d8f0c4b07731
Kevin Hughes <kevinhughes27@gmail.com>
2017-09-17 20:33:34 -0400

    backfill deleted records

--- db/migrate/20170916170038_reports_home_away_scores_null_constraint.rb
+++ db/migrate/20170916170038_reports_home_away_scores_null_constraint.rb
@@ -1,5 +1,25 @@
 class ReportsHomeAwayScoresNullConstraint < ActiveRecord::Migration[5.0]
   def change
+    # oops I forgot about the default scope in my backfill.
+    ScoreReport.only_deleted.find_each do |report|
+      home_score = if report.team == report.game.home
+        report.team_score
+      else
+        report.opponent_score
+      end
+
+      away_score = if report.team == report.game.home
+        report.opponent_score
+      else
+        report.team_score
+      end
+
+      report.update_columns(
+        home_score: home_score,
+        away_score: away_score
+      )
+    end
+
     change_column :score_reports, :home_score, :integer, null: false
     change_column :score_reports, :away_score, :integer, null: false
   end
