6200c286c3d9ecf6e68bc38b98d81002af7202c7
Kevin Hughes <kevinhughes27@gmail.com>
2017-09-17 20:43:25 -0400

    purge soft deleted score reports

--- db/migrate/20170916170038_reports_home_away_scores_null_constraint.rb
+++ db/migrate/20170916170038_reports_home_away_scores_null_constraint.rb
@@ -1,25 +1,10 @@
 class ReportsHomeAwayScoresNullConstraint < ActiveRecord::Migration[5.0]
   def change
     # oops I forgot about the default scope in my backfill.
-    ScoreReport.only_deleted.find_each do |report|
-      home_score = if report.team == report.game.home
-        report.team_score
-      else
-        report.opponent_score
-      end
-
-      away_score = if report.team == report.game.home
-        report.opponent_score
-      else
-        report.team_score
-      end
-
-      report.update_columns(
-        home_score: home_score,
-        away_score: away_score
-      )
-    end
-
+      # they can't be backfilled because the game is deleted in most cases.
+      # this limits the use of soft deleted score reports. I am deleting them for
+      # real on Production now since I don't need them for anything (they have short term value)
+    ScoreReport.only_deleted.delete_all!
     change_column :score_reports, :home_score, :integer, null: false
     change_column :score_reports, :away_score, :integer, null: false
   end
