b058ac2b775f0643fb8e281764f39a4834df8011
Kevin Hughes <kevinhughes27@gmail.com>
2017-09-18 23:22:57 -0400

    unregister the service worker when entering the admin

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -18,14 +18,28 @@ const isLocalhost = Boolean(
     )
 );
 
+const isAdmin = Boolean(!window.location.pathname.match('/admin'));
+
 export default function register() {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location);
+
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
       // serve assets; see https://github.com/facebookincubator/create-react-app/issues/2374
+      return;
+    }
+
+    // don't run the service worker for the admin
+    if (!isAdmin) {
+      // remove the worker and reload the page
+      navigator.serviceWorker.ready.then(registration => {
+        registration.unregister().then(() => {
+          window.location.reload();
+        });
+      });
       return;
     }
 
