cd0d68c406c03f223de394fd40d17e069a0404bf
Kevin Hughes <kevinhughes27@gmail.com>
2017-09-18 21:54:36 -0400

    add support for deepLinking teamName. this closes #50

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
+import { connect } from 'react-redux';
+import queryString from 'query-string';
 
 import Loader from './Loader';
 import ScheduleView from './ScheduleView';
@@ -7,6 +9,10 @@ import MapView from './MapView';
 import SubmitView from './SubmitView';
 
 class App extends Component {
+  componentWillMount() {
+    teamDeepLink(this.props.params, this.props.dispatch);
+  }
+
   render() {
     return (
       <Loader>
@@ -18,4 +24,14 @@ class App extends Component {
   }
 }
 
-export default App;
+function teamDeepLink(params, dispatch) {
+  const teamName = params['teamName'];
+
+  if (teamName) {
+    dispatch({ type: 'SET_SEARCH', value: teamName });
+  }
+}
+
+export default connect(state => ({
+  params: queryString.parse(state.router.location.search)
+}))(App);
