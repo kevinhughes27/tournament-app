e54682757f1609d45917401489f52891b35b97e1
Kevin Hughes <kevinhughes27@gmail.com>
2017-11-12 23:12:42 -0500

    convert stages to use instances instead of classes

--- lib/bracket_db2/structure.rb
+++ lib/bracket_db2/structure.rb
@@ -8,9 +8,21 @@ module BracketDb
       @stages = []
     end
 
-    def stage(stage_type, &block)
-      new_stage = { stage_type.to_s => stage_type.instance_eval(&block) }
+    def stage(type, &block)
+      new_stage = { type => build_stage(type).instance_eval(&block) }
       @stages << new_stage
+    end
+
+    private
+
+    def build_stage(type)
+      stage_types = {
+        pools: PoolStage,
+        bracket: BracketStage,
+        results: ResultsStage
+      }
+
+      stage_types[type].new
     end
   end
 end
--- lib/bracket_db2/test.rb
+++ lib/bracket_db2/test.rb
@@ -4,16 +4,16 @@ require 'pp'
 
 
 BracketDb.define 'USAU_8.1' do
-  stage PoolStage do
+  stage :pools do
     pool 'A', '4.1', %w(1 3 6 8)
     pool 'B', '4.1', %w(2 4 5 7)
   end
 
-  stage BracketStage do
+  stage :bracket do
     bracket '8.1', %w(A1 B4 B2 A3 A2 B3 B1 A4)
   end
 
-  stage ResultsStage do
+  stage :results do
     %w(W1 L1 W3 L3 W5 L5 W7 L7)
   end
 end
