d11e3ee791299db4f230d0275f006d5eedf6e95b
Kevin Hughes <kevinhughes27@gmail.com>
2018-03-21 16:26:05 -0600

    pass current user to in graphql context. (current_user is nil if not authenticated)

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -6,12 +6,14 @@ class GraphqlController < ApplicationController
   def create
     query_string = params[:query]
     query_variables = params[:variables]
-    context = { tournament: @tournament }
 
     result = Schema.execute(
       query_string,
       variables: query_variables,
-      context: context
+      context: {
+        tournament: @tournament,
+        current_user: current_user
+      }
     )
 
     render json: result
