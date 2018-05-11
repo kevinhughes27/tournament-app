32c491be2b7038c77da98ddb6e32879d04a8e8cd
Kevin Hughes <kevinhughes27@gmail.com>
2018-05-11 12:20:04 -0400

    add the rest of the simple mutations to the API

---
+++ app/graphql/resolvers/map_update.rb
@@ -0,0 +1,16 @@
+class Resolvers::MapUpdate < Resolver
+  def call(inputs, ctx)
+    params = inputs.to_h
+    params['edited_at'] = Time.now
+
+    if ctx[:tournament].map.update(params)
+      {
+        success: true,
+      }
+    else
+      {
+        success: false,
+      }
+    end
+  end
+end
