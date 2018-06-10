0c61f758d258ad0a1dee642b4f618b394c1780dc
Kevin Hughes <kevinhughes27@gmail.com>
2018-06-10 23:34:51 +0200

    add dropdown for user in top bar

--- clients/admin_next/src/App.tsx
+++ clients/admin_next/src/App.tsx
@@ -5,6 +5,8 @@ import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Avatar from '@material-ui/core/Avatar';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 import { withStyles, WithStyles } from '@material-ui/core/styles';
 import withRoot from './withRoot';
@@ -16,9 +18,6 @@ const styles = {
   flex: {
     flex: 1,
   },
-  avatar: {
-    margin: 10,
-  },
   menuButton: {
     marginLeft: -12,
     marginRight: 20,
@@ -28,24 +27,68 @@ const styles = {
 
 interface Props extends WithStyles<typeof styles> {}
 
-function App(props: Props) {
-  const { classes } = props;
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="title" color="inherit" className={classes.flex}>
-            Ultimate Tournament
-          </Typography>
-          <Avatar alt="Kevin Hughes" src="https://www.gravatar.com/avatar/a14e0880b9ef8720734a7db6b6c4ade0" className={classes.avatar} />
-          Kevin Hughes
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+interface State {
+  anchorEl: string | undefined;
+};
+
+class App extends React.Component<Props, State> {
+  public state = {
+    anchorEl: undefined,
+  };
+
+  public handleMenu = (event: any) => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  public handleClose = () => {
+    this.setState({ anchorEl: undefined });
+  };
+
+  public render () {
+    const { classes } = this.props;
+    const { anchorEl } = this.state;
+    const open = Boolean(anchorEl);
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="title" color="inherit" className={classes.flex}>
+              Ultimate Tournament
+            </Typography>
+            <div>
+              <IconButton
+                onClick={this.handleMenu}
+                color="inherit"
+              >
+                <Avatar alt="Kevin Hughes" src="https://www.gravatar.com/avatar/a14e0880b9ef8720734a7db6b6c4ade0" />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={open}
+                onClose={this.handleClose}
+              >
+                <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+                <MenuItem onClick={this.handleClose}>My account</MenuItem>
+              </Menu>
+            </div>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
 }
 
 export default withRoot(withStyles(styles)<{}>(App));
