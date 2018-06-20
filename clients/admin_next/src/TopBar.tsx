fbf9e664d3adb0a71002e23cb134a86c5038b497
Kevin Hughes <kevinhughes27@gmail.com>
2018-06-20 22:34:07 +0200

    extract UserMenu from TopBar

--- clients/admin_next/src/TopBar.tsx
+++ clients/admin_next/src/TopBar.tsx
@@ -4,9 +4,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import Avatar from '@material-ui/core/Avatar';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
+import UserMenu from './UserMenu';
 
 import { withStyles, WithStyles } from '@material-ui/core/styles';
 import withRoot from './withRoot';
@@ -24,27 +22,9 @@ const styles = {
 
 interface Props extends WithStyles<typeof styles> {}
 
-interface State {
-  anchorEl: string | undefined;
-};
-
-class TopBar extends React.Component<Props, State> {
-  public state = {
-    anchorEl: undefined,
-  };
-
-  public handleMenu = (event: any) => {
-    this.setState({ anchorEl: event.currentTarget });
-  };
-
-  public handleClose = () => {
-    this.setState({ anchorEl: undefined });
-  };
-
+class TopBar extends React.Component<Props> {
   public render () {
     const { classes } = this.props;
-    const { anchorEl } = this.state;
-    const open = Boolean(anchorEl);
 
     return (
         <AppBar position="static">
@@ -55,31 +35,7 @@ class TopBar extends React.Component<Props, State> {
                 <Typography variant="title" color="inherit" className={classes.flex}>
                     Ultimate Tournament
                 </Typography>
-                <div>
-                    <IconButton
-                    onClick={this.handleMenu}
-                    color="inherit"
-                    >
-                    <Avatar alt="Kevin Hughes" src="https://www.gravatar.com/avatar/a14e0880b9ef8720734a7db6b6c4ade0" />
-                    </IconButton>
-                    <Menu
-                    id="menu-appbar"
-                    anchorEl={anchorEl}
-                    anchorOrigin={{
-                        vertical: 'top',
-                        horizontal: 'right',
-                    }}
-                    transformOrigin={{
-                        vertical: 'top',
-                        horizontal: 'right',
-                    }}
-                    open={open}
-                    onClose={this.handleClose}
-                    >
-                    <MenuItem onClick={this.handleClose}>Profile</MenuItem>
-                    <MenuItem onClick={this.handleClose}>My account</MenuItem>
-                    </Menu>
-                </div>
+                <UserMenu />
             </Toolbar>
         </AppBar>
     );
---
+++ clients/admin_next/src/UserMenu.tsx
@@ -0,0 +1,74 @@
+import * as React from 'react';
+import IconButton from '@material-ui/core/IconButton';
+import Avatar from '@material-ui/core/Avatar';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+import { withStyles, WithStyles } from '@material-ui/core/styles';
+import withRoot from './withRoot';
+
+const styles = {
+  flex: {
+    flex: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+
+interface Props extends WithStyles<typeof styles> {}
+
+interface State {
+  anchorEl: string | undefined;
+};
+
+class UserMenu extends React.Component<Props, State> {
+  public state = {
+    anchorEl: undefined,
+  };
+
+  public handleMenu = (event: any) => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  public handleClose = () => {
+    this.setState({ anchorEl: undefined });
+  };
+
+  public render () {
+    const { anchorEl } = this.state;
+    const open = Boolean(anchorEl);
+
+    return (
+        <div>
+            <IconButton
+            onClick={this.handleMenu}
+            color="inherit"
+            >
+            <Avatar alt="Kevin Hughes" src="https://www.gravatar.com/avatar/a14e0880b9ef8720734a7db6b6c4ade0" />
+            </IconButton>
+            <Menu
+            id="menu-appbar"
+            anchorEl={anchorEl}
+            anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            open={open}
+            onClose={this.handleClose}
+            >
+            <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+            <MenuItem onClick={this.handleClose}>My account</MenuItem>
+            </Menu>
+        </div>
+    );
+  }
+}
+
+export default withRoot(withStyles(styles)<{}>(UserMenu));
