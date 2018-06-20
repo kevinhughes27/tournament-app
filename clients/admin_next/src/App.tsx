cd803016b314ddb6e94072410de8d2167fe4de4d
Kevin Hughes <kevinhughes27@gmail.com>
2018-06-20 22:30:56 +0200

    extract TopBar from App

--- clients/admin_next/src/App.tsx
+++ clients/admin_next/src/App.tsx
@@ -1,12 +1,5 @@
 import * as React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import Avatar from '@material-ui/core/Avatar';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
+import TopBar from './TopBar';
 
 import { withStyles, WithStyles } from '@material-ui/core/styles';
 import withRoot from './withRoot';
@@ -14,78 +7,19 @@ import withRoot from './withRoot';
 const styles = {
   root: {
     flexGrow: 1,
-  },
-  flex: {
-    flex: 1,
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
+  }
 };
 
 
 interface Props extends WithStyles<typeof styles> {}
 
-interface State {
-  anchorEl: string | undefined;
-};
-
-class App extends React.Component<Props, State> {
-  public state = {
-    anchorEl: undefined,
-  };
-
-  public handleMenu = (event: any) => {
-    this.setState({ anchorEl: event.currentTarget });
-  };
-
-  public handleClose = () => {
-    this.setState({ anchorEl: undefined });
-  };
-
+class App extends React.Component<Props> {
   public render () {
     const { classes } = this.props;
-    const { anchorEl } = this.state;
-    const open = Boolean(anchorEl);
 
     return (
       <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="title" color="inherit" className={classes.flex}>
-              Ultimate Tournament
-            </Typography>
-            <div>
-              <IconButton
-                onClick={this.handleMenu}
-                color="inherit"
-              >
-                <Avatar alt="Kevin Hughes" src="https://www.gravatar.com/avatar/a14e0880b9ef8720734a7db6b6c4ade0" />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={open}
-                onClose={this.handleClose}
-              >
-                <MenuItem onClick={this.handleClose}>Profile</MenuItem>
-                <MenuItem onClick={this.handleClose}>My account</MenuItem>
-              </Menu>
-            </div>
-          </Toolbar>
-        </AppBar>
+        <TopBar />
       </div>
     );
   }
