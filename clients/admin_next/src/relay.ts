d9440b0c81883e285b285ce303ad99236728e4fa
Kevin Hughes <kevinhughes27@gmail.com>
2018-06-26 12:03:34 +0200

    implement relay modern caching (http://taiki-t.hatenablog.com/entry/2017/09/05/181931)

--- clients/admin_next/src/relay.ts
+++ clients/admin_next/src/relay.ts
@@ -1,14 +1,32 @@
-import {
-  Environment,
-  Network,
-  RecordSource,
-  Store
-} from "relay-runtime";
+import { Environment, Network, RecordSource, Store } from "relay-runtime";
+import RelayQueryResponseCache from "relay-runtime/lib/RelayQueryResponseCache";
+
+const cache = new RelayQueryResponseCache({ size: 250, ttl: 60 * 5 * 1000 });
 
 const fetchQuery = (
   operation: any,
-  variables: any
+  variables: any,
+  cacheConfig: any
 ) => {
+
+  const queryID = operation.name;
+
+  const cachedData = cache.get(queryID, variables);
+
+  // Handle force option in RefetchOptions
+  // See: https://facebook.github.io/relay/docs/pagination-container.html
+  // https://facebook.github.io/relay/docs/refetch-container.html
+  const forceLoad = cacheConfig && cacheConfig.force;
+
+  if (!forceLoad && cachedData) {
+    return cachedData;
+  }
+
+  if (forceLoad) {
+    // clear() means to reset all the cache, not only the entry addressed by specific queryId.
+    cache.clear();
+  }
+
   return fetch("/graphql", {
     method: "POST",
     headers: {
@@ -21,6 +39,12 @@ const fetchQuery = (
     }),
   }).then((response) => {
     return response.json();
+  }).then((data) => {
+    if (operation.operationKind !== "mutation") {
+      cache.set(queryID, variables, data);
+    }
+
+    return data;
   });
 };
 
