371782b382a672dfa713182fad1695b77e2c96fe
Kevin Hughes <kevinhughes27@gmail.com>
2018-07-08 14:20:37 +0200

    update to 11.0.9

--- config/initializers/react_on_rails.rb
+++ config/initializers/react_on_rails.rb
@@ -1,12 +1,9 @@
 # Shown below are the defaults for configuration
 ReactOnRails.configure do |config|
-  # Directory where your generated assets go
-  config.generated_assets_dir = File.join(%w[public webpack], Rails.env)
+  config.node_modules_location = "client"
 
   # Define the files for we need to check for webpack compilation when running tests
   config.webpack_generated_files = %w( admin-bundle.js vendor-bundle.js )
-
-  config.node_modules_location = "client"
 
   # This is the file used for server rendering of React when using `(prerender: true)`
   # If you are never using server rendering, you may set this to "".
