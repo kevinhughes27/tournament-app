ced39afeceba3873baa023476089363d98d52a6f
Kevin Hughes <kevinhughes27@gmail.com>
2018-07-11 10:19:12 +0200

    refactor team deepLink to TopBar (more locally located)

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,8 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import queryString from 'query-string';
-
 import Loader from './Loader';
 import Routes from './Routes';
 
 class App extends Component {
-  componentWillMount() {
-    teamDeepLink(this.props.params, this.props.dispatch);
-  }
-
   render() {
     return (
       <Loader>
@@ -19,14 +12,4 @@ class App extends Component {
   }
 }
 
-function teamDeepLink(params, dispatch) {
-  const teamName = params['teamName'];
-
-  if (teamName) {
-    dispatch({ type: 'SET_SEARCH', value: teamName });
-  }
-}
-
-export default connect(state => ({
-  params: queryString.parse(state.router.location.search)
-}))(App);
+export default App;
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import queryString from 'query-string';
+
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import IconButton from 'material-ui/IconButton';
@@ -8,6 +10,10 @@ import AutoComplete from './AutoComplete';
 import Typography from 'material-ui/Typography';
 
 class TopBar extends Component {
+  componentWillMount() {
+    teamDeepLink(this.props.params, this.props.dispatch);
+  }
+
   render() {
     const { teams, search, dispatch } = this.props;
     const teamNames = teams.map(t => t.name);
@@ -34,7 +40,16 @@ class TopBar extends Component {
   }
 }
 
+function teamDeepLink(params, dispatch) {
+  const teamName = queryString.parse(params)['teamName'];
+
+  if (teamName) {
+    dispatch({ type: 'SET_SEARCH', value: teamName });
+  }
+}
+
 export default connect(state => ({
-  teams: state.tournament.teams,
-  search: state.search
+  params: state.router.location.search,
+  search: state.search,
+  teams: state.tournament.teams
 }))(TopBar);
