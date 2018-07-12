355e29d61faf3a41a1480d7ca59cad8e120c3534
Kevin Hughes <kevinhughes27@gmail.com>
2018-07-12 16:48:22 +0200

    use current timezone for factory

--- test/factories/tournament_factory.rb
+++ test/factories/tournament_factory.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :tournament do
     name { Faker::Company.name }
     handle { Faker::Internet.domain_word }
-    timezone 'America/New_York'
+    timezone { Time.zone }
     welcome_email_sent true
     game_confirm_setting 'single'
   end
