b0e998ac4955bef8decec4473ea0e95e716b9a47
Kevin Hughes <kevinhughes27@gmail.com>
2018-07-13 10:39:06 +0200

    refactor form code internals for clarity

--- clients/admin_next/src/views/Teams/TeamForm.tsx
+++ clients/admin_next/src/views/Teams/TeamForm.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Formik } from "formik";
+import { Formik, FormikValues, FormikProps, FormikActions } from "formik";
 
 import { withStyles, WithStyles } from "@material-ui/core/styles";
 import { Form as styles } from "../../assets/jss/styles";
@@ -29,83 +29,87 @@ class TeamForm extends React.Component<Props, State> {
     errors: undefined
   };
 
+  onSubmit = (values: FormikValues, actions: FormikActions<FormikValues>) => {
+    UpdateTeamMutation.commit(
+      environment,
+      values,
+      this.props.team,
+      (response, errors) => {
+        const result = response.updateTeam;
+
+        if (result.success) {
+          this.setState({message: "Team Saved"});
+        } else if (errors) {
+          this.setState({errors: ["Invalid Input"]});
+        } else {
+          this.setState({errors: result.userErrors});
+        }
+      }
+    );
+  }
+
   render() {
-    const { team, divisions, classes } = this.props;
+    const { team, classes } = this.props;
+
+    const initialValues = {
+      name: team.name,
+      email: team.email,
+      divisionId: team.division.id,
+      seed: team.seed
+    };
 
     return (
       <div className={classes.container}>
         <Toast message={this.state.message} />
         <Errors errors={this.state.errors} />
         <Formik
-          initialValues={{
-            name: team.name,
-            email: team.email,
-            divisionId: team.division.id,
-            seed: team.seed
-          }}
-          onSubmit={(
-            values,
-          ) => {
-            UpdateTeamMutation.commit(
-              environment,
-              values,
-              this.props.team,
-              (response, errors) => {
-                const result = response.updateTeam;
-
-                if (result.success) {
-                  this.setState({message: "Team Saved"});
-                } else if (errors) {
-                  this.setState({errors: ["Invalid Input"]});
-                } else {
-                  this.setState({errors: result.userErrors});
-                }
-              }
-            );
-          }}
-          render={({
-            values,
-            handleChange,
-            handleSubmit,
-          }) => (
-            <form onSubmit={handleSubmit}>
-              <TextField
-                id="name"
-                label="Name"
-                margin="normal"
-                fullWidth
-                value={values.name}
-                onChange={handleChange}
-              />
-              <TextField
-                id="email"
-                label="Email"
-                margin="normal"
-                fullWidth
-                value={values.email || ""}
-                onChange={handleChange}
-              />
-              <DivisionPicker
-                divisionId={values.divisionId}
-                divisions={divisions}
-                onChange={handleChange}
-              />
-              <TextField
-                id="seed"
-                label="Seed"
-                margin="normal"
-                fullWidth
-                type="number"
-                value={values.seed}
-                onChange={handleChange}
-              />
-              <Button variant="contained" color="primary" type="submit" className={classes.button}>
-                Save
-              </Button>
-            </form>
-          )}
+          initialValues={initialValues}
+          onSubmit={this.onSubmit}
+          render={this.renderForm}
         />
       </div>
+    );
+  }
+
+  renderForm = ({values, handleChange, handleSubmit}: FormikProps<FormikValues>) => {
+    const { divisions, classes } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit}>
+        <TextField
+          id="name"
+          label="Name"
+          margin="normal"
+          fullWidth
+          value={values.name}
+          onChange={handleChange}
+        />
+        <TextField
+          id="email"
+          label="Email"
+          margin="normal"
+          fullWidth
+          value={values.email || ""}
+          onChange={handleChange}
+        />
+        <DivisionPicker
+          divisionId={values.divisionId}
+          divisions={divisions}
+          onChange={handleChange}
+        />
+        <TextField
+          id="seed"
+          label="Seed"
+          margin="normal"
+          fullWidth
+          type="number"
+          value={values.seed}
+          onChange={handleChange}
+        />
+        <Button variant="contained" color="primary" type="submit" className={classes.button}>
+          Save
+        </Button>
+      </form>
     );
   }
 }
