57c240dcac9d4497039304d9f9e33491f63f61c7
Kevin Hughes <kevinhughes27@gmail.com>
2018-07-15 10:39:35 +0200

    fix lowercase search. add and refactor tests

--- test/browser/player_app_browser_test.rb
+++ test/browser/player_app_browser_test.rb
@@ -6,91 +6,117 @@ class PlayerAppBrowserTest < BrowserTest
     @map = FactoryBot.create(:map, tournament: @tournament)
     @team = FactoryBot.create(:team, name: 'Swift')
     @opponent = FactoryBot.create(:team, name: 'Goose')
-    @game1 = FactoryBot.create(:game, :scheduled, home: @team, away: @opponent)
+    @game = FactoryBot.create(:game, :scheduled, home: @team, away: @opponent)
   end
 
   test 'submit a score' do
-    visit("http://no-borders.#{Settings.host}/")
-
-    # search for team
-    fill_in(placeholder: 'Search Teams', with: 'Swift')
-
-    # navigate to submit score screen
-    click_on('Submit Score')
-
-    # click on game to submit score
-    click_on('Swift vs Goose')
-
-    # fill out form
-    fill_in('homeScore', with: 15)
-    fill_in('awayScore', with: 11)
-    click_on('Submit')
+    visit_app
+    search_for_team('Swift')
+    navigate_to_submit
+    click_on_game(@game)
+    enter_score(15, 11)
+    submit_score
 
     assert_submitted
-
-    report = ScoreReport.last
-    assert_equal 'Swift', report.team.name
-    assert_equal @game1, report.game
-    assert_equal 15, report.home_score
-    assert_equal 11, report.away_score
-    assert report.submitter_fingerprint
+    assert_report('Swift', @game, 15, 11)
   end
 
   test 'submit a score (with pin)' do
     @tournament.update_column(:score_submit_pin, '1234')
 
-    visit("http://no-borders.#{Settings.host}/")
-
-    # search for team
-    fill_in(placeholder: 'Search Teams', with: 'Swift')
-
-    # navigate to submit score screen
-    click_on('Submit Score')
-
-    # enter pin
-    input = first('input.pincode-input-text')
-    input.send_keys '1234'
-
-    # click on game to submit score
-    click_on('Swift vs Goose')
-
-    # fill out form
-    fill_in('homeScore', with: 15)
-    fill_in('awayScore', with: 11)
-    click_on('Submit')
+    visit_app
+    search_for_team('Swift')
+    navigate_to_submit
+    enter_pin('1234')
+    click_on_game(@game)
+    enter_score(15, 11)
+    submit_score
 
     assert_submitted
+    assert_report('Swift', @game, 15, 11)
+  end
 
-    report = ScoreReport.last
-    assert_equal 'Swift', report.team.name
-    assert_equal @game1, report.game
-    assert_equal 15, report.home_score
-    assert_equal 11, report.away_score
-    assert report.submitter_fingerprint
+  test 'submit a score (lowercase search)' do
+    visit_app
+    search_for_team('swift')
+    navigate_to_submit
+    click_on_game(@game)
+    enter_score(15, 11)
+    submit_score
+
+    assert_submitted
+    assert_report('Swift', @game, 15, 11)
   end
 
   test 'deep link' do
-    report = FactoryBot.create(:score_report, game: @game1, team: @team)
+    report = FactoryBot.create(:score_report, game: @game, team: @team)
 
     deep_link = report.build_confirm_link
 
     visit(deep_link)
-
-    fill_in('comments', with: 'Yup thats the score')
-    click_on('Submit')
+    enter_comment('Yup thats the score')
+    submit_score
 
     assert_submitted
+    assert_confirmed_report(report, comment: 'Yup thats the score')
+  end
 
+  private
+
+  def visit_app
+    visit("http://#{@tournament.handle}.#{Settings.host}/")
+  end
+
+  def search_for_team(value)
+    fill_in(placeholder: 'Search Teams', with: value)
+  end
+
+  def navigate_to_submit
+    click_on('Submit Score')
+  end
+
+  def enter_pin(pin)
+    input = first('input.pincode-input-text')
+    input.send_keys pin
+  end
+
+  def click_on_game(game)
+    click_on("#{game.home_name} vs #{game.away_name}")
+  end
+
+  def enter_score(home, away)
+    fill_in('homeScore', with: home)
+    fill_in('awayScore', with: away)
+  end
+
+  def enter_comment(text)
+    fill_in('comments', with: text)
+  end
+
+  def submit_score
+    click_on('Submit')
+  end
+
+  def assert_submitted
+    assert page.find("svg[color='green']")
+  end
+
+  def assert_report(team, game, home, away)
+    report = ScoreReport.last
+    assert_equal team, report.team.name
+    assert_equal game, report.game
+    assert_equal home, report.home_score
+    assert_equal away, report.away_score
+    assert report.submitter_fingerprint
+  end
+
+  def assert_confirmed_report(report, comment:)
     confirmed_report = ScoreReport.last
     assert_equal confirmed_report.team.name, report.other_team.name
     assert_equal confirmed_report.game, report.game
     assert_equal confirmed_report.home_score, report.home_score
     assert_equal confirmed_report.away_score, report.away_score
-    assert confirmed_report.comments, 'Yup thats the score'
+    assert confirmed_report.comments, comment
     assert confirmed_report.submitter_fingerprint
-  end
-
-  def assert_submitted
-    assert page.find("svg[color='green']")
   end
 end
