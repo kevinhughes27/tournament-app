331988208eeb81b10d4edd92353f443895150c5f
Hitesh <hitesh.gurjar786@gmail.com>
2018-09-20 20:04:21 +0530

    test case update

--- test/browser/user_profile_test.rb
+++ test/browser/user_profile_test.rb
@@ -1,50 +1,6 @@
 require 'test_helper'
 
 class SettingBrowserTest < BrowserTest
-  setup do
-    @tournament = FactoryBot.create(:tournament, handle: 'no-borders')
-    @user = FactoryBot.create(:user)
-    FactoryBot.create(:tournament_user, user: @user, tournament: @tournament)
-  end
-
-  test 'user change password' do
-    visit_app
-    login
-    navigate_to_profile
-    logout
-  end
-
-  private
-
-  def visit_app
-    visit("http://#{@tournament.handle}.#{Settings.host}/admin_next")
-  end
-
-  def login
-    assert_text 'Log in to manage your tournament'
-
-    fill_in('email', with: @user.email)
-    fill_in('password', with: 'password')
-    click_on('Log in')
-
-    assert_text('Home')
-  end
-
-  def navigate_to_profile
-    find("img[alt='#{@user.email}']").click
-    click_text('Profile')
-  end
-
-
-
-  def logout
-    find("img[alt='#{@user.email}']").click
-    click_text('Logout')
-    assert_text 'Log in to manage your tournament'
-  end
-
-  def click_text(text)
-    page.find(:xpath,"//*[text()='#{text}']").click
-  end
+ 
 
 end
