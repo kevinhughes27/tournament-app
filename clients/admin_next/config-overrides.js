809e7615a4e7e8187d782c261ca87e927be2fdb2
Kevin Hughes <kevinhughes27@gmail.com>
2018-10-10 16:43:58 +0200

    remove thread loader config

--- clients/admin_next/config-overrides.js
+++ clients/admin_next/config-overrides.js
@@ -9,17 +9,6 @@ module.exports = {
     config = rewireTypescript(config);
     config = rewireTSLint(config);
     config = injectBabelPlugin(["relay", { "artifactDirectory": "./src/generated" }], config);
-
-    if (process.env.CI) {
-      const threadLoader = config.module.rules[3].oneOf[1].use[0];
-      config.module.rules[3].oneOf[1].use[0] = {
-        loader: threadLoader,
-        options: {
-          workers: process.env.CIRCLE_NODE_TOTAL
-        }
-      }
-    }
-
     return config;
   }
 };
