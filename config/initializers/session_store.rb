9c0d15b5ec24a1a0f6dbdbfbb999cec8633b61e7
Kevin Hughes <kevinhughes27@gmail.com>
2018-11-25 17:25:07 +0900

    fix typo in rails session variable

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 Rails.application.config.session_store :cookie_store,
-  key: '_ultimate-tournamet_session',
+  key: '_ultimate-tournament_session',
   domain: Settings.host.gsub(':3000', '')
