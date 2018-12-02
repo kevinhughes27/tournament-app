8b4e249e0fdbb91ec60b69da3857057ce202be8f
Kevin Hughes <kevinhughes27@gmail.com>
2018-12-02 21:47:17 +0900

    fix test deprecation

--- test/api/viewer_query_test.rb
+++ test/api/viewer_query_test.rb
@@ -3,24 +3,28 @@ require 'test_helper'
 class ViewerQueryTest < ApiTest
   test "viewer is null" do
     viewer = FactoryBot.create(:user)
+
     query_graphql("
       viewer {
       	name
         email
       }",
     )
-     assert_equal query_result["viewer"], nil
+
+     assert_nil query_result["viewer"]
   end
 
   test "email field is present for authenticated requests" do
     viewer = FactoryBot.create(:user)
     login_user(viewer)
+
     query_graphql("
       viewer {
       	name
         email
       }"
     )
+
     assert_equal viewer.email,  query_result['viewer']['email']
   end
 end
