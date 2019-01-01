4e01bd62d459fe3876860e89e659e02f71d81f0e
Kevin Hughes <kevinhughes27@gmail.com>
2019-01-01 16:15:54 +0800

    team factory email fallback

--- test/factories/team_factory.rb
+++ test/factories/team_factory.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :team do
     tournament { Tournament.first || FactoryBot.build(:tournament) }
     name { Faker::Team.unique.name }
-    email { "#{name.parameterize}@example.com" }
+    email { name ? "#{name.parameterize}@example.com" : Faker::Internet.unique.email }
   end
 end
