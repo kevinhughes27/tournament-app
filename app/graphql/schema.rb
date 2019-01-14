bf231ad7211b95b970f9ddfee4f62dbff3add9fe
Kevin Hughes <kevinhughes27@gmail.com>
2019-01-14 11:32:53 +0700

    warm schema trick to fix duplicate definition issues. (https://github.com/rmosolgo/graphql-ruby/issues/1505)

--- app/graphql/schema.rb
+++ app/graphql/schema.rb
@@ -8,3 +8,8 @@ class Schema < GraphQL::Schema
   use GraphQL::Batch
   use GraphQL::Subscriptions::ActionCableSubscriptions
 end
+
+# This processes the schema members and makes some caches on the schema,
+# maybe it's subject to a race condition?
+# So eagerly warm that cache at load time:
+Schema.graphql_definition
