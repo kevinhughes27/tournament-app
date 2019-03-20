fd5405740b14be577c2eddbbf5f78384ae0f9f47
Kevin Hughes <kevinhughes27@gmail.com>
2019-03-20 12:15:02 +0700

    fix iframe locally

--- src/reducers/location.js
+++ src/reducers/location.js
@@ -1,9 +1,11 @@
 import ReactGA from 'react-ga';
 
 export default function(state = {}, action) {
+  const host = window.location.hostname;
+
   switch (action.type) {
     case '@@router/LOCATION_CHANGE':
-      if (window.location.hostname !== 'localhost') {
+      if (host !== 'localhost' && host !== 'lvh.me') {
         ReactGA.set({ page: window.location.pathname });
         ReactGA.pageview(window.location.pathname);
       }
