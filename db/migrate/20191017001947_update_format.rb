2cd649455d5755539a5c5618974e0cc9bf52eeed
Kevin Hughes <kevinhughes27@gmail.com>
2019-10-16 20:22:01 -0400

    migration to update formats

---
+++ db/migrate/20191017001947_update_format.rb
@@ -0,0 +1,13 @@
+class UpdateFormat < ActiveRecord::Migration[5.2]
+  def change
+    bracket = BracketDb.find(handle: "CUUC Women 2019 Div II")
+
+    Division.where(bracket_type: "CUUC Women 2019 Div II").find_each do |division|
+      ChangeBracketJob.perform_now(
+        tournament_id: division.tournament_id,
+        division_id: division.id,
+        new_template: bracket.template
+      )
+    end
+  end
+end
